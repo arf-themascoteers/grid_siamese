0eadfee7f421429d1524d8ac25f1a3ddc5509e95
arif <arif@themascoteers.com>
2023-10-01 13:26:56 +1100

    done

--- test_skip.py
+++ test_skip.py
@@ -1,7 +1,7 @@
 from evaluator import Evaluator
 
 if __name__ == "__main__":
-    c = Evaluator(prefix="ann_learnable_avg_skip", folds=10, algorithms=[
+    c = Evaluator(prefix="ann_learnable_avg_skip2", folds=10, algorithms=[
         "ann_learnable_avg_skip"
     ])
     c.process()
