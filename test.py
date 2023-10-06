5cc277f87173e6c4c60b07150dcb39f1c558afc8
Arif <arif@themascoteers.com>
2023-10-06 13:14:01 +1100

    done

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from evaluator import Evaluator
 
 if __name__ == "__main__":
-    c = Evaluator(prefix="position", folds=10, algorithms=[
-        "ann_centre_only","ann_top_left_only","ann_avg","ann_centric_avg","ann_learnable_avg"
+    c = Evaluator(prefix="xy1", folds=10, algorithms=[
+        "ann_centre_only","ann_top_left_only","ann_avg",
+        "ann_centric_avg","ann_learnable_avg",
+        "ann_learnable_avg_skip"
     ])
     c.process()
